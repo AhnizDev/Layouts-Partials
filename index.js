58a0606f353a77c4d6e8fc9738e48f4ce05c18bf
AhnizDigital <ahnizdigital@gmail.com>
2025-10-06 12:38:29 +0100

    AhnizDigital Layouts-Partials Version 1.1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3000;
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index.ejs");
+  console.log("Home page accessed");
 });
 
 app.get("/about", (req, res) => {
